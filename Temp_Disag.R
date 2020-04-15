4732bae43d7cd2a511409169d759791c32726a00
Mauricio Montiel <mauricioo.moontiel@icloud.com>
2020-04-14 21:56:49 -0500

    other

--- Temp_Disag.R
+++ Temp_Disag.R
@@ -1,15 +1,35 @@
+################################################
+# Con este código se pueden pronósticar series de tiempo multivariadas
 #
-## Desagregación temporal del PIB        
+## Funciones de transferencia para pronosticar el IPC utilizando el precio del petróleo
 #
+# Hecho por Mauricio Montiel el 14/04/2020
+###############################################
 
-pib <- read.csv(paste(path, "PIB.csv", sep = ""), row.names = 1)
+#librerías
+library(readxl)
+library(tidyverse)
+library(tseries)
+library(urca)
+library(forecast)
+library(tempdisagg)
 
-pib <- ts(pib, start = c(as.numeric(year), (as.numeric(month)-1)/3), 
+# Preliminares
+remove(list = ls())
+setwd("~/GitHub/Forecasting/Desagregacion-Temporal-PIB")
+
+# Importar datos
+datos <- read.csv("PIB.csv")
+pib <- ts(datos[,1], start = c(1997, 2), end=c(2019,3), 
           frequency = 4)
 
-pib_m <- predict(td(pib ~ datos[,"igae"] + 0, conversion = "average", 
-                    to = "monthly", method = "denton-cholette"))
+igae <- ts(datos[,2], start = c(1997, 7), 
+          frequency = 12)
+x11()
+ts.plot(igae)
 
+pib_m <- predict(td(pib ~ igae + 0, conversion = "average", 
+                    to = "monthly", method = "denton-cholette"))
 
 # Grafica del PIB mensual
 ts.plot(pib_m)
